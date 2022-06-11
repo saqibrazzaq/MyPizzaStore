b0e2f329d7c110be67d7babbc26a759fba42b4f6
saqibrazzaq <saqibrazzaq@gmail.com>
2022-06-11 12:26:13 +0500

    added react project web ui typescript support template chakra ui

---
+++ web-ui/src/App.tsx
@@ -0,0 +1,26 @@
+import * as React from "react"
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
+} from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import TopNavbar from "./components/header/top-navbar"
+
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <TopNavbar />
+        <VStack spacing={8}>
+          
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
